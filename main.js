996d3bce56fc9d4972226344faa2b143874a4e4c
sanma yoshi <hakusaiandyoshi@outlook.com>
2024-12-05 19:00:29 +0900

    タブのの追加、その他微修正

--- main.js
+++ main.js
@@ -7,18 +7,33 @@ function sleepSetTimeout(ms, callback) {
 
 //OP用
 function OP() {
+
+	function EndOP() {
+		OP.className = "";
+		header.style = "display:flex;";
+		contents.style = "display:block;";
+	}
+
 	var OP = document.getElementById("OP");
 	var OP_text = document.getElementById("op_text");
 	var OP_front = document.getElementById("op_front");
 	var header = document.getElementById("header");
 	var contents = document.getElementById("contents");
+
+	if (!sessionStorage.getItem('firstVisit')) {
+		// 初回訪問の場合、セッションストレージにフラグをセット
+		sessionStorage.setItem('firstVisit', 'true');
+	} else {
+		EndOP();
+		return 0;
+	}
+
 	OP.className = "Playing";
 	OP_text.className = "Playing";
 	OP_front.className = "Playing";
+
 	sleepSetTimeout(3000, function () {
-		OP.className = "";
-		header.style = "display:flex;";
-		contents.style = "display:block;";
+		EndOP();
 });
 }
 
