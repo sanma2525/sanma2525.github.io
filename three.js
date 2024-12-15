2ed2f713b394c8ebb04dc623148030cbc5b4f580
sanma2525 <hakusaiandyoshi@outlook.com>
2024-12-15 19:13:42 +0900

    FPS制限されないバグあり、デバッグ

--- three.js
+++ three.js
@@ -24,21 +24,26 @@ const camera = new THREE.PerspectiveCamera(45, width / height);
 camera.position.set(0, 0, +1000);
 
 // 箱を作成
-const geometry = new THREE.BoxGeometry(300,300,300);
-const material = new THREE.MeshNormalMaterial();
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
 
-const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
-scene.add(light);
+class Box {
+    method() {
+        function createBox(var sizex,var sizey, var sizez) {
+            const geometry = new THREE.BoxGeometry(300, 300, 300);
+        const material = new THREE.MeshNormalMaterial();
+        const box = new THREE.Mesh(geometry, material);
+        scene.add(box);
 
-console.log(box.position.x);
-console.log(box.position.y);
+        const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
+        scene.add(light);
 
+        console.log(box.position.x);
+        console.log(box.position.y);
+    }
+}
+}
 let lastUpdateTime = performance.now(); // 前回のフレームの時間
 
 function animate() {
-    requestAnimationFrame(animate);
 
     const now = performance.now();
     const deltaTime = now - lastUpdateTime;
@@ -53,14 +58,14 @@ function animate() {
     // フレーム更新の処理を実行する
     // この中に描画処理やアニメーションなどを記述します
     // 例えば、controlsの更新を呼び出してカメラの位置を更新する場合は以下のようにします
-
-    requestAnimationFrame(animate);
     // アニメーション処理をここに書く
     box.rotation.y += 0.01;
     box.rotation.x += 0.01;
-    box.position.y +=  4 * Math.sin(num);
+    box.position.y += 4 * Math.sin(num);
     num += 0.1;
     renderer.render(scene, camera);
+
+    requestAnimationFrame(animate);
 }
 
 animate();
